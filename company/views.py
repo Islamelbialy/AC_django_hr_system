cef69f379bbbacfe40a25f4d3abedd4fc45748ce
islamelbialy <islam.mohamed@mans.edu.eg>
2024-10-26 18:11:01 +0300

    lesson 3

--- company/views.py
+++ company/views.py
@@ -4,28 +4,32 @@ from .models import Branches
 
 # Create your views here.
 
+# def BranchesView(request):
+#    branches  = Branches.objects.all()
+#    htmlSTR = """
+#    <table>
+#       <thead>
+#          <th>name</th>
+#          <th>address</th>
+#          <th>email</th>
+#       </thead>
+#    """
+#    for branche in branches:
+#       htmlSTR += f"""
+#          <tr>
+#             <td>{branche.name}</td>
+#             <td>{branche.address}</td>
+#             <td>{branche.email}</td>
+#          </tr>
+#       """
+   
+#    htmlSTR += "</table>"
+
+#    return HttpResponse(htmlSTR)
+
 def BranchesView(request):
    branches  = Branches.objects.all()
-   htmlSTR = """
-   <table>
-      <thead>
-         <th>name</th>
-         <th>address</th>
-         <th>email</th>
-      </thead>
-   """
-   for branche in branches:
-      htmlSTR += f"""
-         <tr>
-            <td>{branche.name}</td>
-            <td>{branche.address}</td>
-            <td>{branche.email}</td>
-         </tr>
-      """
-   
-   htmlSTR += "</table>"
-
-   return HttpResponse(htmlSTR)
+   return render(request,"BranchesList.html",{"branches":branches})
 
 def view(request):
    return HttpResponse("<h1>view 2</h1>")
